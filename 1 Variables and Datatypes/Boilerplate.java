29eecbaff1af15b551b1981a0e72de041f3a7e18
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-01 00:51:20 +0600

    new weapone java

---
+++ 1 Variables and Datatypes/Boilerplate.java
@@ -0,0 +1,22 @@
+public class Boilerplate {
+
+    public static void main(String[] args) {
+    
+   }
+
+}
+
+// in avobe templete called boileplate
+
+
+//public-- for all 
+//clas--- all works for java
+//class name = 2_Boilerplate
+
+
+//clss name and file name must be same
+//ex : avobe
+
+
+
+// main function : ( main ) compiler find the main function and work..
